20f52fd3260535cfbb5ac9f5b1cfe8af67db5b48
llahorgue01 <llahorgue01@SEC-C210-F07.csip.in>
2025-04-03 09:35:48 +0200

    ajouté Getters, Setters, Constructeur à la classe Operateur

--- src/main/java/fr/insa/hupka/atelier/Operateur.java
+++ src/main/java/fr/insa/hupka/atelier/Operateur.java
@@ -13,4 +13,47 @@ public class Operateur {
     private String nom;
     private String prenom;
     private boolean libre;
+    //Constructeur
+
+    public Operateur(String code, String nom, String prenom, boolean libre) {
+        this.code = code;
+        this.nom = nom;
+        this.prenom = prenom;
+        this.libre = libre;
+    }
+    //Getter
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public boolean isLibre() {
+        return libre;
+    }
+    //Setter
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setLibre(boolean libre) {
+        this.libre = libre;
+    }
+    
 }
