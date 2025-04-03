2220c084599a161cd717384975822b27e80f0f69
llahorgue01 <llahorgue01@SEC-C210-F07.csip.in>
2025-04-03 09:27:21 +0200

    ajouté Getters, Setters, Constructeur à la classe Equipement

--- src/main/java/fr/insa/hupka/atelier/Equipement.java
+++ src/main/java/fr/insa/hupka/atelier/Equipement.java
@@ -9,7 +9,30 @@ package fr.insa.hupka.atelier;
  * @author phupka01
  */
 public class Equipement {
+    //attributs
     private String refEquipement;
     private String dEquipement;
-   
+    //Constructeur
+    public Equipement(String refEquipement, String dEquipement) {
+        this.refEquipement = refEquipement;
+        this.dEquipement = dEquipement;
+    }
+   //Getters
+    public String getRefEquipement() {
+        return refEquipement;
+    }
+
+    public String getdEquipement() {
+        return dEquipement;
+    }
+    //Setters
+
+    public void setRefEquipement(String refEquipement) {
+        this.refEquipement = refEquipement;
+    }
+
+    public void setdEquipement(String dEquipement) {
+        this.dEquipement = dEquipement;
+    }
+    
 }
