f90427d45e05b9e8c54827e25513e560f85d70cf
phupka01 <pierre.hupka@insa-strasbourg.fr>
2025-04-03 09:22:19 +0200

    etat initial

--- src/main/java/fr/insa/hupka/atelier/Machine.java
+++ src/main/java/fr/insa/hupka/atelier/Machine.java
@@ -15,5 +15,57 @@ public class Machine {
     private float cout;
     private float x;
     private float y;
+
+    
+    
+    public String getRefMachine() {
+        return refMachine;
+    }
+
+    public void setRefMachine(String refMachine) {
+        this.refMachine = refMachine;
+    }
+
+    public String getdMachine() {
+        return dMachine;
+    }
+
+    public void setdMachine(String dMachine) {
+        this.dMachine = dMachine;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public float getCout() {
+        return cout;
+    }
+
+    public void setCout(float cout) {
+        this.cout = cout;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+    
     
 }
