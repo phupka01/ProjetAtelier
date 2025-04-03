c4051f57f1811060d01aa3af0bce5e34445eb8db
llahorgue01 <llahorgue01@SEC-C210-F07.csip.in>
2025-04-03 09:54:15 +0200

    ajouté Getters, Setters, Constructeur, quelques Méthodes  à la classe Gamme. /!\ terminer l'ajout des méthodes !

--- src/main/java/fr/insa/hupka/atelier/Gamme.java
+++ src/main/java/fr/insa/hupka/atelier/Gamme.java
@@ -4,10 +4,56 @@
  */
 package fr.insa.hupka.atelier;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author phupka01
  */
 public class Gamme {
-    
+    //attributs
+    private String RefGamme;
+    private ArrayList<Operation> ListeOperations;
+    private ArrayList<Equipement> ListeEquipements;
+    //Constructeur
+
+    public Gamme(String RefGamme, ArrayList<Operation> ListeOperations, ArrayList<Equipement> ListeEquipements) {
+        this.RefGamme = RefGamme;
+        this.ListeOperations = ListeOperations;
+        this.ListeEquipements = ListeEquipements;
+    }
+    //Getters
+
+    public String getRefGamme() {
+        return RefGamme;
+    }
+
+    public ArrayList<Operation> getListeOperations() {
+        return ListeOperations;
+    }
+
+    public ArrayList<Equipement> getListeEquipements() {
+        return ListeEquipements;
+    }
+    //Setters
+
+    public void setRefGamme(String RefGamme) {
+        this.RefGamme = RefGamme;
+    }
+
+    public void setListeOperations(ArrayList<Operation> ListeOperations) {
+        this.ListeOperations = ListeOperations;
+    }
+
+    public void setListeEquipements(ArrayList<Equipement> ListeEquipements) {
+        this.ListeEquipements = ListeEquipements;
+    }
+    //Méthodes
+     //Méthodes de gestion
+    public void creerGamme(){
+        
+    }
+    public void modifierGamme(){
+        
+    }    
 }
