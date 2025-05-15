30e56e513e6e280c8e2bf0f7168f3c2136a1a546
llahorgue01 <llahorgue01@PC-0290.csip.in>
2025-05-15 12:32:11 +0200

    Création de la classe Fiabilite chargée de réaliser les rapports de fiabilité

---
+++ src/main/java/fr/insa/hupka/atelier/Fiabilite.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.insa.hupka.atelier;
+
+
+
+/**
+ *
+ * @author llahorgue01
+ */
+public class Fiabilite {
+   private Machine[] tbmachine = new Machine[50];
+   private float[] tbdates = new float[50];
+   private float[] tbheure = new float[50];
+   private Operateur[] tboperateur = new Operateur[50];
+   private char[] tbevent = new char[50];
+   private String[] tbcause = new String[50];
+   
+   //Constructeur 
+    //GetSet
+    public Machine[] getTbmachine() {
+        return tbmachine;
+    }
+
+    public void setTbmachine(Machine[] tbmachine) {
+        this.tbmachine = tbmachine;
+    }
+
+    public float[] getTbdates() {
+        return tbdates;
+    }
+
+    public void setTbdates(float[] tbdates) {
+        this.tbdates = tbdates;
+    }
+
+    public float[] getTbheure() {
+        return tbheure;
+    }
+
+    public void setTbheure(float[] tbheure) {
+        this.tbheure = tbheure;
+    }
+
+    public Operateur[] getTboperateur() {
+        return tboperateur;
+    }
+
+    public void setTboperateur(Operateur[] tboperateur) {
+        this.tboperateur = tboperateur;
+    }
+
+    public char[] getTbevent() {
+        return tbevent;
+    }
+
+    public void setTbevent(char[] tbevent) {
+        this.tbevent = tbevent;
+    }
+
+    public String[] getTbcause() {
+        return tbcause;
+    }
+
+    public void setTbcause(String[] tbcause) {
+        this.tbcause = tbcause;
+    }
+    //Méthodes
+
+    
+   
+}
